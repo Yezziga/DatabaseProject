28eb4a0b8b84207e0e31cb93bfa442a462b726b9
JessicaQ <jessica_quach60@hotmail.com>
2018-11-05 16:15:36 +0100

    made gui fix bookingpnl!

--- DatabaseProject/src/frontend/RegisterPnl.java
+++ DatabaseProject/src/frontend/RegisterPnl.java
@@ -13,7 +13,7 @@ public class RegisterPnl extends JPanel {
 	private JLabel lblAddress = new JLabel("Address");
 	private JLabel lblEmail = new JLabel("Email");
 	private JLabel lblPhone = new JLabel("Phone");
-	private JLabel lblID = new JLabel("Your traveler-ID");
+//	private JLabel lblID = new JLabel("Your traveler-ID");
 	private JLabel lblStatus = new JLabel("status comes here");
 
 	private JLabel invis = new JLabel();
@@ -23,14 +23,14 @@ public class RegisterPnl extends JPanel {
 	private JLabel invis4 = new JLabel();
 	private JLabel invis5 = new JLabel();
 	private JLabel invis6 = new JLabel();
-	private JLabel invis7 = new JLabel();
-	private JLabel invis8 = new JLabel();
+//	private JLabel invis7 = new JLabel();
+//	private JLabel invis8 = new JLabel();
 
-	private JTextField tfName = new JTextField("testa");
-	private JTextField tfAddress = new JTextField("testa");
-	private JTextField tfEmail = new JTextField("testa");
-	private JTextField tfPhone = new JTextField("testa");
-	private JTextField tfID = new JTextField("testa");
+	private JTextField tfName = new JTextField();
+	private JTextField tfAddress = new JTextField();
+	private JTextField tfEmail = new JTextField();
+	private JTextField tfPhone = new JTextField();
+//	private JTextField tfID = new JTextField();
 
 	private JButton bnRegister = new JButton("Register");
 	private JButton bnCancel = new JButton("Cancel");
@@ -40,56 +40,57 @@ public class RegisterPnl extends JPanel {
 	public RegisterPnl() {
 		setLayout(new BorderLayout());
 
-		setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+//		setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		tfName.setPreferredSize(new Dimension(160, 20));
 		tfAddress.setPreferredSize(new Dimension(160, 20));
 		tfEmail.setPreferredSize(new Dimension(160, 20));
 		tfPhone.setPreferredSize(new Dimension(160, 20));
-		tfID.setPreferredSize(new Dimension(160, 20));
+//		tfID.setPreferredSize(new Dimension(160, 20));
 
-		invis.setPreferredSize(new Dimension(440, 20));
-		invis1.setPreferredSize(new Dimension(250, 20));
-		invis2.setPreferredSize(new Dimension(537, 20));
-		invis3.setPreferredSize(new Dimension(348, 20));
-		invis4.setPreferredSize(new Dimension(348, 20));
+		invis.setPreferredSize(new Dimension(610, 90));
+		invis1.setPreferredSize(new Dimension(610, 20));
+		invis2.setPreferredSize(new Dimension(610, 20));
+		invis3.setPreferredSize(new Dimension(610, 20));
+		invis4.setPreferredSize(new Dimension(610, 20));
 
 		invis5.setPreferredSize(new Dimension(800, 20));
 		invis6.setPreferredSize(new Dimension(800, 20));
-		invis7.setPreferredSize(new Dimension(800, 20));
-		invis8.setPreferredSize(new Dimension(800, 20));
-		bnRegister.setLocation(600, 600);
+//		invis7.setPreferredSize(new Dimension(800, 20));
+//		invis8.setPreferredSize(new Dimension(800, 20));
 		
 		panel.setPreferredSize(new Dimension(300, 70));
-		panel.setBorder(BorderFactory.createLineBorder(Color.RED, 5));
+//		panel.setBorder(BorderFactory.createLineBorder(Color.RED));
 
+//		invis1.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+
+		panel.setBackground(Color.WHITE);
 		add(panel, BorderLayout.CENTER);
 
 		panel.add(invis);
 		panel.add(lblName);
 		panel.add(tfName);
 		panel.add(invis1);
-//		pnlLeft.add(invis5);
 
 		panel.add(lblAddress);
 		panel.add(tfAddress);
 		panel.add(invis2);
-//		pnlLeft.add(invis6);
 
 		panel.add(lblEmail);
 		panel.add(tfEmail);
 		panel.add(invis3);
-//		pnlLeft.add(invis7);
 
 		panel.add(lblPhone);
 		panel.add(tfPhone);
 		panel.add(invis4);
-		// add(invis8);
 
-		panel.add(lblID);
-		panel.add(tfID);
+//		panel.add(lblID);
+//		panel.add(tfID);
+		panel.add(invis5);
 
+		panel.add(bnRegister);
 		panel.add(bnCancel);
-		add(lblStatus);
+		panel.add(invis6);
+		panel.add(lblStatus);
 	}
 
 }
