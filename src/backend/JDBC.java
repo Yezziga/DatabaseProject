fb1817b6e9cef5356f6120630dea82059d45a5da
Gimpan <jimmyquach@live.com>
2018-11-06 00:31:18 +0100

    Made JDBC static/singleton for connection only.

--- src/backend/JDBC.java
+++ src/backend/JDBC.java
@@ -2,41 +2,29 @@ package backend;
 
 import java.sql.*;
 
-public class JDBC {
-	private final String url = "jdbc:postgresql://pgserver.mah.se/ah7658project"; // change which database to connect to
-	private final String user = "ah7658";
-	private final String password = "aer0jpco";
-	private Connection conn = null;
-	
-	public JDBC() {
-		this.conn = connect();
-		InsertQueries.getDrivers(app);
+public final class JDBC {
+
+	private JDBC() {
+		conn = getConnection();
 	}
-	
-	public Connection connect() {
-		Connection conn = null;
+
+	final static String url = "jdbc:postgresql://pgserver.mah.se/ah7658project"; // change which database to connect to
+	final static String user = "ah7658";
+	final static String password = "aer0jpco";
+	private static Connection conn = null;
+
+	public static Connection getConnection() {
+
 		try {
-			conn = DriverManager.getConnection(url, user, password);
-			System.out.println("Connected to the PostgreSQL server successfully.");
+			if (conn.isClosed() || conn == null) {
+
+				conn = DriverManager.getConnection(url, user, password);
+				System.out.println("Connected to the PostgreSQL server successfully.");
+			}
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 
 		return conn;
-	}	
-	
-//	public static class GetQueries {
-//		public void GetSetWow()
-//		{
-//			
-//		}
-//	}
-//	
-//	public static class InsertQueries {
-//		
-//	}
-//
-//	public static class UpdateQueries {
-//		
-//	}
+	}
 }
