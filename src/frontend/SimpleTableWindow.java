4a7183d1f8012bcfb5527c48ba2f43efa26f83b2
Gimpan <jimmyquach@live.com>
2018-11-06 14:11:12 +0100

    Added actionlisteners

--- src/frontend/SimpleTableWindow.java
+++ src/frontend/SimpleTableWindow.java
@@ -7,17 +7,26 @@ import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import javax.swing.event.*;
+import javax.swing.table.TableModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SimpleTableWindow extends JPanel {
+
+public class SimpleTableWindow extends JPanel implements TableModelListener, ActionListener {
 
 	private boolean DEBUG = false;
-
+	private JTable table;
+	
 	public SimpleTableWindow() {super(new GridLayout(1,0));
 
-	        JTable table = new JTable(new MyTableModel());
+	        table = new JTable(new MyTableModel());
 	        table.setPreferredScrollableViewportSize(new Dimension(500, 70));
 	        table.setFillsViewportHeight(true);
-
+	        
+	        table.getModel().addTableModelListener(this);
 	        //Create the scroll pane and add the table to it.
 	        JScrollPane scrollPane = new JScrollPane(table);
 
@@ -27,11 +36,13 @@ public class SimpleTableWindow extends JPanel {
 
 	class MyTableModel extends AbstractTableModel {
 		private String[] columnNames = { "First Name", "Last Name", "Sport", "# of Years", "Vegetarian" };
-		private Object[][] data = { { "Kathy", "Smith", "Snowboarding", new Integer(5), new Boolean(false) },
+		private Object[][] data = {
+				{ "Kathy", "Smith", "Snowboarding", new Integer(5), new Boolean(false) },
 				{ "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
 				{ "Sue", "Black", "Knitting", new Integer(2), new Boolean(false) },
 				{ "Jane", "White", "Speed reading", new Integer(20), new Boolean(true) },
-				{ "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) } };
+				{ "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) }
+				};
 
 		public int getColumnCount() {
 			return columnNames.length;
@@ -131,5 +142,68 @@ public class SimpleTableWindow extends JPanel {
 				createAndShowGUI();
 			}
 		});
+	}
+
+	@Override
+	public void tableChanged(TableModelEvent e) {
+		int row = e.getFirstRow();
+        int column = e.getColumn();
+        TableModel model = (TableModel)e.getSource();
+        String columnName = model.getColumnName(column);
+        Object data = model.getValueAt(row, column);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		String command = event.getActionCommand();
+        //Cell selection is disabled in Multiple Interval Selection
+        //mode. The enabled state of cellCheck is a convenient flag
+        //for this status.
+//        if ("Row Selection" == command) {
+//            table.setRowSelectionAllowed(rowCheck.isSelected());
+//            //In MIS mode, column selection allowed must be the
+//            //opposite of row selection allowed.
+//            if (!cellCheck.isEnabled()) {
+//                table.setColumnSelectionAllowed(!rowCheck.isSelected());
+//            }
+//        } else if ("Column Selection" == command) {
+//            table.setColumnSelectionAllowed(columnCheck.isSelected());
+//            //In MIS mode, row selection allowed must be the
+//            //opposite of column selection allowed.
+//            if (!cellCheck.isEnabled()) {
+//                table.setRowSelectionAllowed(!columnCheck.isSelected());
+//            }
+//        } else if ("Cell Selection" == command) {
+//            table.setCellSelectionEnabled(cellCheck.isSelected());
+//        } else if ("Multiple Interval Selection" == command) { 
+//            table.setSelectionMode(
+//                    ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+//            //If cell selection is on, turn it off.
+//            if (cellCheck.isSelected()) {
+//                cellCheck.setSelected(false);
+//                table.setCellSelectionEnabled(false);
+//            }
+//            //And don't let it be turned back on.
+//            cellCheck.setEnabled(false);
+//        } else if ("Single Interval Selection" == command) {
+//            table.setSelectionMode(
+//                    ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+//            //Cell selection is ok in this mode.
+//            cellCheck.setEnabled(true);
+//        } else if ("Single Selection" == command) {
+//            table.setSelectionMode(
+//                    ListSelectionModel.SINGLE_SELECTION);
+//            //Cell selection is ok in this mode.
+//            cellCheck.setEnabled(true);
+//        }
+// 
+//        //Update checkboxes to reflect selection mode side effects.
+//        rowCheck.setSelected(table.getRowSelectionAllowed());
+//        columnCheck.setSelected(table.getColumnSelectionAllowed());
+//        if (cellCheck.isEnabled()) {
+//            cellCheck.setSelected(table.getCellSelectionEnabled());
+//        }
+		
 	}
 }
