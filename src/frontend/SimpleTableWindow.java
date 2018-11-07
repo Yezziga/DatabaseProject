33d816ea7134726455c41eb447d6fe77625ad029
Gimpan <jimmyquach@live.com>
2018-11-07 11:35:55 +0100

    Removed comments

--- src/frontend/SimpleTableWindow.java
+++ src/frontend/SimpleTableWindow.java
@@ -71,21 +71,18 @@ public class SimpleTableWindow extends JPanel implements TableModelListener, Act
 	}
 
 	class MyTableModel extends AbstractTableModel {
-		// private String[] columnNames = { "First Name", "Last Name", "Sport", "# of
-		// Years", "Vegetarian" };
-		// private Object[][] data = {
-		// { "Kathy", "Smith", "Snowboarding", new Integer(5), new Boolean(false) },
-		// { "John", "Doe", "Rowing", new Integer(3), new Boolean(true) },
-		// { "Sue", "Black", "Knitting", new Integer(2), new Boolean(false) },
-		// { "Jane", "White", "Speed reading", new Integer(20), new Boolean(true) },
-		// { "Joe", "Brown", "Pool", new Integer(10), new Boolean(false) }
-		// };
+
 		private String[] columnNames;
 		private Object[][] data;
 
 		public MyTableModel() {
 			this.columnNames = getColumns();
 			this.data = getData();
+		}
+		
+		public MyTableModel(String[] columnNames, Object[][] data) {
+			this.columnNames = columnNames;
+			this.data = data;
 		}
 
 		public int getColumnCount() {
